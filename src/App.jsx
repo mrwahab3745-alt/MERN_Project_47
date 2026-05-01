e0915a647f87acd2a740b14254e3cda19ff500c9
mrwahab3745-alt <mrwahab3745@gmail.com>
2026-05-01 18:23:19 +0500

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './Pages/about'
 import Home from './Pages/Home'
-import Profile from './Pages/Profile'
+import Contact from './Pages/Contact'
 import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
+import Header from './components/Header';
+
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <h1 className='text-red-700 font-bold text-2xl'>Hello, World!</h1>
+      <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-      </div>
     </BrowserRouter>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center gap-4 p-2 max-w-6xl mx-auto">
+        <Link to="/" className="text-2xl font-bold text-black">
+          <h1 className="text-sm sm:text-xl font-bold flex">
+          <span className="text-slate-500 ">Real</span>
+          <span className="text-black">Estate</span>
+        </h1>
+        </Link>
+        <form className="bg-slate-100 rounded-2xl flex items-center gap-2 px-4 py-1">
+          <input
+            type="search"
+            placeholder="Search Here"
+            className="bg-transparent focus:outline-none w-24 sm:w-48"
+          /><FaSearch className="text-slate-500" />
+        </form>
+        <ul className="flex flex-wrap gap-5">
+          <li className="hidden sm:inline text-black hover:underline">
+            <a href="/">Home</a>
+          </li>
+          <li className="hidden sm:inline text-black hover:underline">
+            <a href="/about">About</a>
+          </li>
+          <li className="hidden sm:inline text-black hover:underline">
+            <a href="/Contact">Contact</a>
+          </li>
+          <li className="sm:inline text-black hover:underline">
+            <a href="/signin">Sign In</a>
+          </li>
+          <li className="sm:inline text-black hover:underline">
+            <a href="/signup">Sign Up</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}

b377eae90d70ceeffb6106c304145fa380139b78
mrwahab3745-alt <mrwahab3745@gmail.com>
2026-05-03 14:30:34 +0500

    created a test API route.

--- API/index.js
+++ API/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dns from "node:dns";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config({ quiet: true });
 
@@ -63,3 +64,5 @@ connectToDatabase().then(() => {
     console.log("Server is running on port 3000");
   });
 });
+
+app.use("/api/user", userRoutes);
